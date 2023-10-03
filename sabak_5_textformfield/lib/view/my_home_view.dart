c841d4e953b42606035a25721555d5d60d7c6576
Aizirek Bakirova <aizirek.bakirova@gmail.com>
2023-10-03 21:03:52 +0600

    done

---
+++ sabak_5_textformfield/lib/view/my_home_view.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+class MyHomeView extends StatefulWidget {
+  const MyHomeView({super.key});
+
+  @override
+  State<MyHomeView> createState() => _MyHomeViewState();
+}
+
+class _MyHomeViewState extends State<MyHomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff056c5c),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        title: const Text("Tapshyrma 4"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(50),
+              child: Image.asset("assets/images/fish.png"),
+            ),
+            const Text(
+              "Aizirek Bakirova",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 48,
+                  fontWeight: FontWeight.w400,
+                  fontFamily: "Pacifico-Regular"),
+            ),
+            const Text(
+              "Flutter Developer",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 28,
+                  fontWeight: FontWeight.w400),
+            ),
+            const Divider(
+              color: Colors.white,
+              indent: 25,
+              endIndent: 25,
+              height: 0,
+              thickness: 2,
+            ),
+            const SizedBox(
+              height: 23,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                fillColor: Colors.white,
+                filled: true,
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                fillColor: Colors.white,
+                filled: true,
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
