b8633a7887e943dd445c3416b7ffbfdd4b2f3911
Aizirek Bakirova <aizirek.bakirova@gmail.com>
2023-12-15 18:05:58 +0600

    done

--- sabak_21_capitals_of_the_world02/lib/features/repositories/pages/detail_page.dart
+++ sabak_21_capitals_of_the_world02/lib/features/repositories/pages/detail_page.dart
@@ -4,32 +4,39 @@ import 'package:sabak_20_capitals_of_the_world1/features/repositories/theme/text
 class DetailPage extends StatelessWidget {
   const DetailPage({super.key, required this.image});
   final String image;
+  final double sl = 10;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('22', style: AppTextStyle.numberFalseStyle),
-            Text('|'),
-            Text(
-              '10',
-              style: AppTextStyle.numberTrueStyle,
-            )
-          ],
-        ),
-        actions: [const Icon(Icons.more_vert)],
-      ),
+      appBar: _detailAppBar(),
       body: Column(
         children: [
-          Image.asset(
-            image,
-            width: 160,
+          Slider(
+            value: sl,
+            onChanged: (value) {},
+            min: 0.0,
+            max: 100.0,
           )
         ],
       ),
+    );
+  }
+
+  AppBar _detailAppBar() {
+    return AppBar(
+      title: const Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('22', style: AppTextStyle.numberFalseStyle),
+          Text('|'),
+          Text(
+            '10',
+            style: AppTextStyle.numberTrueStyle,
+          )
+        ],
+      ),
+      actions: [const Icon(Icons.more_vert)],
     );
   }
 }
