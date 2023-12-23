917bb2e2395d53493d25764e4154834545767a63
Aizirek Bakirova <aizirek.bakirova@gmail.com>
2023-12-23 23:58:40 +0600

    in progress

---
+++ sabak_21_capitals_of_the_world02/lib/features/domain/model/test.dart
@@ -0,0 +1,11 @@
+class Test {
+  final String capitalName;
+  final String capitalImage;
+
+  Test({required this.capitalName, required this.capitalImage});
+}
+
+class Joop {
+  final String countriesName;
+  final
+}
