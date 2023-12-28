bc5032b313d58302af0e3e7d88a312aac647bb91
Aizirek Bakirova <aizirek.bakirova@gmail.com>
2023-12-28 16:24:51 +0600

    done

--- sabak_24_weather_app_2_get_data/lib/doc.dart
+++ sabak_24_weather_app_2_get_data/lib/doc.dart
@@ -2,3 +2,5 @@
 // 1) download package Dio (to get the request and response)
 // 2) import package Dio
 // 3) create a function to get data from server with Dio
+// 4) connect data with status 200 to the model
+// 5) display information on the screens
