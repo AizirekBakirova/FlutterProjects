48c660d10a05e21b64bacad34d0f9bddd7eb36c8
Aizirek Bakirova <aizirek.bakirova@gmail.com>
2023-12-28 16:19:26 +0600

    done

---
+++ sabak_24_weather_app_2_get_data/lib/doc.dart
@@ -0,0 +1,4 @@
+// procedure of working with data server
+// 1) download package Dio (to get the request and response)
+// 2) import package Dio
+// 3) create a function to get data from server with Dio
