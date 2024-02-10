ad97102a0d98bc642723e5bb0d51f4b6fa0d83f0
Aizirek Bakirova <aizirek.bakirova@gmail.com>
2024-02-10 13:41:16 +0600

    done

---
+++ sabak_34_firebase_todo_1/lib/features/presentation/pages/home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Home Page'),
+      ),
+    );
+  }
+}
